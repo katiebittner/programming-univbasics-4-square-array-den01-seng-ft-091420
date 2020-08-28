46334518a5011941d974624880f1b05013bdb7c2
Katie B <katiebittner1@gmail.com>
2020-08-28 17:50:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
-def square_array(array)
+def square_array(array) {
   counter = 0 
   square_array = []
-  while counter<array.length 
+  while counter<array.length {
     square_array.push(array[counter]**2)
     counter+=1
-  end
+  }
   square_array
-end
+}
